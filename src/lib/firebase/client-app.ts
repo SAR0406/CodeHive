f5f768cfaede411238fbb2ee04a49d53c7bd9a83
Sarthak Upadhyay <sarthak040624@gmail.com>
2025-09-22 12:30:25 +0000

    npm install firebase then
    // Import the functions you need from the SD

--- src/lib/firebase/client-app.ts
+++ src/lib/firebase/client-app.ts
@@ -3,13 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyBJuPhG0OIuTe4Rj6ASt9MvVMSSGf4yY6s",
+  authDomain: "studio-8735887866-b7f30.firebaseapp.com",
+  projectId: "studio-8735887866-b7f30",
+  storageBucket: "studio-8735887866-b7f30.appspot.com",
+  messagingSenderId: "722888612466",
+  appId: "1:722888612466:web:ba1516da168634a89ec0eb"
 };
 
 // Initialize Firebase
