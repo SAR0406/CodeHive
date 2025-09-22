17cc5528e3f747b086bc8bc3ff4399815fa46363
Sarthak Upadhyay <sarthak040624@gmail.com>
2025-09-22 15:46:46 +0000

    recode ai genkit in new way use not server

--- src/ai/dev.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/suggest-code-fixes.ts';
-import '@/ai/flows/explain-code-snippet.ts';
-import '@/ai/flows/generate-tests-from-code.ts';
-import '@/ai/flows/generate-app-from-prompt.ts';
-import '@/ai/flows/generate-story-flow.ts';
