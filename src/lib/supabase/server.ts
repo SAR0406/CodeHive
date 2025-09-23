997d851aa394778975843981b924317fff0749a4
Sarthak Upadhyay <sarthak040624@gmail.com>
2025-09-23 09:48:33 +0000

    15:09:47.285 Running build in Washington, D.C., USA (East) – iad1 15:09:

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,6 +4,9 @@ import { cookies } from 'next/headers'
 export function createClient() {
   const cookieStore = cookies()
 
+  // NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY are required
+  // to be set in the .env file and in the Vercel environment variables.
+  // if they are not set, the app will throw an error.
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
