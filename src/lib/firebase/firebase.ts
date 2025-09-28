d40e2ea7d5f1e12e99a0f00e822e53ca695a3ce4
Sarthak Upadhyay <sarthak040624@gmail.com>
2025-09-28 16:15:54 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -3,6 +3,7 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 import { getAuth, type Auth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,17 +12,22 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 let app: FirebaseApp;
 let auth: Auth;
 let db: Firestore;
+let analytics;
 
 // This code will only run on the client side
 try {
   app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
   auth = getAuth(app);
   db = getFirestore(app);
+  if (typeof window !== 'undefined') {
+    analytics = getAnalytics(app);
+  }
 } catch (error) {
   console.error("Firebase initialization error:", error);
   // In case of an error, we provide null or dummy objects
@@ -29,6 +35,8 @@ try {
   app = null!; 
   auth = null!;
   db = null!;
+  analytics = null;
 }
 
-export { app, auth, db };
+export { app, auth, db, analytics };
+
